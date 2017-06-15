e011f3e41985c09636b840c0a39747238ea3fc64
sophieklm <sklm@live.co.uk>
2017-06-15 12:38:41 +0100

    moves update responsibility to item class

--- gilded_rose.py
+++ gilded_rose.py
@@ -6,59 +6,59 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if "Aged Brie" in item.name:
-                self.brie_update_quality(item)
+                item.brie_update()
             elif "Sulfuras" in item.name:
                 return
             elif "Backstage passes" in item.name:
-                self.backstage_pass_update_quality(item)
+                item.backstage_pass_update()
             elif "Conjured" in item.name:
-                self.conjured_update_quality(item)
+                item.conjured_update()
             else:
-                self.normal_update_quality(item)
-
-    def normal_update_quality(self, item):
-        item.sell_in -= 1
-        if item.quality == 0:
-            return
-        item.quality -= 1
-        if item.sell_in <= 0:
-            item.quality -= 1
-
-    def brie_update_quality(self, item):
-        item.sell_in -= 1
-        if item.quality >= 50:
-            return
-        item.quality += 1
-        if item.sell_in <= 0:
-            item.quality += 1
-
-    def backstage_pass_update_quality(self, item):
-        item.sell_in -= 1
-        if item.quality == 0 or item.quality == 50:
-            return
-        item.quality += 1
-        if item.sell_in <= 0:
-            item.quality = 0
-            return
-        if item.quality < 50:
-            if item.sell_in <= 10:
-                item.quality += 1
-            if item.sell_in <= 5:
-                item.quality += 1
-
-    def conjured_update_quality(self, item):
-        item.sell_in -= 1
-        if item.quality == 0:
-            return
-        item.quality -= 2
-        if item.sell_in <= 0:
-            item.quality -= 2
+                item.normal_update()
 
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
+
+    def normal_update(self):
+        self.sell_in -= 1
+        if self.quality == 0:
+            return
+        self.quality -= 1
+        if self.sell_in <= 0:
+            self.quality -= 1
+
+    def brie_update(self):
+        self.sell_in -= 1
+        if self.quality >= 50:
+            return
+        self.quality += 1
+        if self.sell_in <= 0:
+            self.quality += 1
+
+    def backstage_pass_update(self):
+        self.sell_in -= 1
+        if self.quality == 0 or self.quality == 50:
+            return
+        self.quality += 1
+        if self.sell_in <= 0:
+            self.quality = 0
+            return
+        if self.quality < 50:
+            if self.sell_in <= 10:
+                self.quality += 1
+            if self.sell_in <= 5:
+                self.quality += 1
+
+    def conjured_update(self):
+        self.sell_in -= 1
+        if self.quality == 0:
+            return
+        self.quality -= 2
+        if self.sell_in <= 0:
+            self.quality -= 2
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
