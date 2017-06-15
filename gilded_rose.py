cdcb360e47d70340e3808edd1e2709f23eb06a51
sophieklm <sklm@live.co.uk>
2017-06-15 15:50:59 +0100

    further refactoring

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,64 +1,69 @@
+def normal_update(item):
+    item.sell_in -= 1
+    if item.quality == 0:
+        return
+    item.quality -= 1
+    if item.sell_in <= 0:
+        item.quality -= 1
+
+def brie_update(item):
+    item.sell_in -= 1
+    if item.quality >= 50:
+        return
+    item.quality += 1
+    if item.sell_in <= 0:
+        item.quality += 1
+
+def backstage_update(item):
+    item.sell_in -= 1
+    if item.quality == 0 or item.quality == 50:
+        return
+    item.quality += 1
+    if item.sell_in <= 0:
+        item.quality = 0
+        return
+    if item.quality < 50:
+        if item.sell_in <= 10:
+            item.quality += 1
+        if item.sell_in <= 5:
+            item.quality += 1
+
+def conjured_update(item):
+    item.sell_in -= 1
+    if item.quality == 0:
+        return
+    item.quality -= 2
+    if item.sell_in <= 0:
+        item.quality -= 2
+
+def sulfuras_update(item):
+    return
+
 class GildedRose(object):
+
+    TYPE = {
+        "Aged Brie": brie_update,
+        "Sulfuras, Hand of Ragnaros": sulfuras_update,
+        "Backstage passes to a TAFKAL80ETC concert": backstage_update,
+        "Conjured Mana Cake": conjured_update
+    }
 
     def __init__(self, items):
         self.items = items
 
+
     def update_quality(self):
         for item in self.items:
-            if "Aged Brie" in item.name:
-                item.brie_update()
-            elif "Sulfuras" in item.name:
-                return
-            elif "Backstage passes" in item.name:
-                item.backstage_pass_update()
-            elif "Conjured" in item.name:
-                item.conjured_update()
-            else:
-                item.normal_update()
+            try:
+                self.TYPE[item.name](item)
+            except KeyError:
+                normal_update(item)
 
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
-
-    def normal_update(self):
-        self.sell_in -= 1
-        if self.quality == 0:
-            return
-        self.quality -= 1
-        if self.sell_in <= 0:
-            self.quality -= 1
-
-    def brie_update(self):
-        self.sell_in -= 1
-        if self.quality >= 50:
-            return
-        self.quality += 1
-        if self.sell_in <= 0:
-            self.quality += 1
-
-    def backstage_pass_update(self):
-        self.sell_in -= 1
-        if self.quality == 0 or self.quality == 50:
-            return
-        self.quality += 1
-        if self.sell_in <= 0:
-            self.quality = 0
-            return
-        if self.quality < 50:
-            if self.sell_in <= 10:
-                self.quality += 1
-            if self.sell_in <= 5:
-                self.quality += 1
-
-    def conjured_update(self):
-        self.sell_in -= 1
-        if self.quality == 0:
-            return
-        self.quality -= 2
-        if self.sell_in <= 0:
-            self.quality -= 2
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
